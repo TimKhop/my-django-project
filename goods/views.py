e2a5d1b67ccb18c8e245a20b2d7b46851239477a
TimKhop <example@mail.com>
2024-04-13 15:23:09 +0400

    After adding fixtures for goods

--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
+
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+    context = {
+        "title": "Спорт Лайн - Каталог",
+        "catalog_cart": [
+            {
+                "image": "deps/images/Велосипед_1.jpeg",
+                "name": "Велосипед"
+            },
+            {
+                "image": "deps/images/Мяч_5.jpeg",
+                "name": "Мячи"
+            },
+            {
+                "image": "deps/images/Кроссовки_2.jpeg",
+                "name": "Кроссовки"
+            },
+            {
+                "image": "deps/images/Тренажер_4.jpeg",
+                "name": "Тренажеры"
+            },
+            {
+                "image": 'deps/images/Спортивный инвентарь_3.jpeg',
+                "name": "Спортивный инвентарь"
+            },
+        ]
+    }
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request):
-    return render(request, 'goods/product.html')
+    return render(request, "goods/product.html")
