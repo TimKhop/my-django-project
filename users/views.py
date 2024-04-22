10e138cf90e3a31d78c0ba83cfd8b44976e534a8
TimKhop <example@mail.com>
2024-04-22 20:11:16 +0400

    Login form and controller are ready

--- users/views.py
+++ users/views.py
@@ -1,11 +1,29 @@
-from calendar import c
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+
+from users.forms import UserLoginForm
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+            
     context = {
-        'title': 'Спорт Лайн - Авторизация'
+        'title': 'Спорт Лайн - Авторизация',
+        'form': form
     }
-    return render(request, '', context)
+    return render(request, 'main/index.html', context)
 
 def registration(request):
     context = {
