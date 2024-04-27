bdc123aca358ede9825855a24e7fd8da713f245b
TimKhop <example@mail.com>
2024-04-27 23:56:41 +0400

    Only modal return mesages

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact_view, name='contact'),
+    # path('contact/', views.contact_view, name='contact'),
     path('contact/', views.handle_contact_form, name='contact'),  # Обработчик формы обратной связи
 ]
