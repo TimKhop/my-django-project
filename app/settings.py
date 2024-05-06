abcbffc0ddf0629467f3a20a6c5048bc3ee18f1b
TimKhop <example@mail.com>
2024-05-06 12:00:22 +0400

    Psycopg2-binary

--- app/settings.py
+++ app/settings.py
@@ -32,12 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv.get("True", "False").lower() == "true"
+DEBUG = os.environ.get("True", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 
