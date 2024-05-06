4442e72e30bae144d4a6f189c7ab7e79e95b2f90
TimKhop <example@mail.com>
2024-05-06 10:41:39 +0400

    Host and Debug

--- app/settings.py
+++ app/settings.py
@@ -32,9 +32,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-django-project-zm20.onrender.com']
+
 
 
 # Application definition
